2d781c233f33beede2dcd8cba61ec4558917cb45
daisukixci <gaetan.piquenot@datadoghq.com>
2024-04-22 10:17:22 +0200

    Add omz install

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+##### Shell configuration
+# Install Oh my zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+##### Vim configuration
