48a27be277f01bcb9ce63a1d15a6d02c7b301fc4
daisukixci <gaetan.piquenot@datadoghq.com>
2024-04-22 12:01:16 +0200

    Add gh cli

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ echo "CONFIG_DIR: $CONFIG_DIR"
 echo "VIM_PLUGINS_DIR: $VIM_PLUGINS_DIR"
 
 ##### Install packages
-apt install -y fzf
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt update
+sudo apt install -y gh fzf
 
 ##### Shell configuration
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > "${HOME}/install-oh-my-zsh.sh"
