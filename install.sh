a44dda3efa0f1713acfd8350c702660f45cc752b
daisukixci <gaetan.piquenot@datadoghq.com>
2024-09-24 12:01:04 +0200

    Add global gitignore

--- install.sh
+++ install.sh
@@ -17,6 +17,7 @@
 
     ##### Shell configuration
     cp "${DOTFILES_DIR}/.gitconfig" "${HOME}"
+    cp "${DOTFILES_DIR}/.gitignore" "${HOME}"
     sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
     git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
